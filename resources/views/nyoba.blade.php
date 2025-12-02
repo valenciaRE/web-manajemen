4b032c0b41276e0eeabb61c19a972dcedcb34c60
valenciaRE <valenciaekasura@gmail.com>
2025-12-02 13:39:02 +0700

    web manajemen

---
+++ resources/views/nyoba.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <h1 class="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+
+</body>
+</html>
